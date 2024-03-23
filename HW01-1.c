1ee4d375c72849e234f91c12c7389453d9b59191
Kimpeace <39222528+KimSnowFlower@users.noreply.github.com>
2024-03-24 00:51:52 +0900

    Update HW01-1.c

--- HW01-1.c
+++ HW01-1.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-void swap(int *a, int *b);
+void swap(int *arr);
 
 int main() {
  int a[2] = {3, 20};
 
  printf("함수사용전:%d,%d\n",a[0],a[1]);
- swap(&a[0],&a[1]);
+ swap(a);
  printf("함수사용후:%d,%d\n",a[0],a[1]);
 
  return 0;
 }
 
-void swap(int *a, int *b) {
- int temp = *a;
- *a = *b;
- *b = temp
+void swap(int *arr) {
+ int temp = arr[0];
+ arr[0] = arr[1];
+ arr[1] = temp
 }
